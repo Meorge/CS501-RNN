b63e96fe0c61e119f3b96c5b5b77f576167d7cee
Malcolm Anderson <malcolminyo@gmail.com>
2023-11-13 11:12:04 -0800

    Model is closer to working now, but linear layer setup is bad

--- model_v2.py
+++ model_v2.py
@@ -2,6 +2,11 @@ import torch.nn as nn
 import torch
 import numpy as np
 
+try:
+    from rich import print
+except ImportError:
+    print("rich not installed, using builtin print")
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -10,9 +15,9 @@ class PrimaryUserPresenceNetwork(nn.Module):
         super().__init__()
         self.num_neighbors = kwargs["num_neighbors"]
 
-        self.self_module = SingleSecondaryUserModule(**kwargs)
+        self.self_module = SingleSecondaryUserModule()
         self.user_modules = [
-            SingleSecondaryUserModule(**kwargs) for _ in range(self.num_neighbors)
+            SingleSecondaryUserModule() for _ in range(self.num_neighbors)
         ]
 
         self.user_modules.insert(0, self.self_module)
@@ -25,38 +30,47 @@ class PrimaryUserPresenceNetwork(nn.Module):
         out = np.zeros(len(self.user_modules))
         for i, module in enumerate(self.user_modules):
             out[i] = module(in_recent_values[i], in_reputation_history[i])
-        
+
         out = self.final_linear(out)
         return out
-        
 
 
 class SingleSecondaryUserModule(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self, hidden_size=128, num_layers=1, out_size=1):
         super().__init__()
-        self.num_history = kwargs["num_history"]
-        self.lstm_hidden_size = kwargs["lstm_hidden_size"]
-        self.lstm_num_layers = kwargs["lstm_num_layers"]
-        self.out_size = kwargs["out_size"]
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.out_size = out_size
 
+        # Input is tensor of shape (L, H_in) for unbatched input
+        # L = sequence length
+        # H_in = input_size (1)
         self.lstm = nn.LSTM(
-            input_size=self.num_history,
-            hidden_size=self.lstm_hidden_size,
-            num_layers=self.lstm_num_layers,
-            batch_first=True,
+            input_size=1,
+            hidden_size=self.hidden_size,
+            num_layers=self.num_layers,
         )
 
         self.linear = nn.Linear(
-            in_features=self.lstm_hidden_size + 1, out_features=self.out_size
+            in_features=self.hidden_size + 1, out_features=self.out_size
         )
 
-    def forward(self, current_reading_input, history_input):
-        hidden = torch.zeros(self.lstm_num_layers, self.lstm_hidden_size).to(device)
-        cells = torch.zeros(self.lstm_num_layers, self.lstm_hidden_size).to(device)
+    def forward(self, current_reading_input: torch.Tensor, history_input: torch.Tensor):
+        hidden = torch.zeros(self.num_layers, self.hidden_size).to(device)
+        cells = torch.zeros(self.num_layers, self.hidden_size).to(device)
 
+        history_input = history_input.unsqueeze(1)
         output, _ = self.lstm(history_input, (hidden, cells))
-
-        linear_in = np.concatenate(output[-1, :], current_reading_input)
+        
+        output: torch.Tensor
+        
+        output = output.flatten()
+        
+        current_reading_input = current_reading_input.unsqueeze(0)
+        print(output, current_reading_input)
+        linear_in = np.concatenate((output, current_reading_input))
+        print(linear_in)
+        
         prediction = self.linear(linear_in)
         return prediction
 
@@ -66,16 +80,20 @@ def train(
     model: nn.Module,
     optimizer: torch.optim.Optimizer,
     criterion: nn.modules.loss._Loss,
-    inputs: tuple[torch.Tensor],
+    input_most_recent_values: torch.Tensor,
+    input_rep_history_for_users: torch.Tensor,
     expected_output: torch.Tensor,
 ):
     for epoch in range(n_epochs):
         print(f"EPOCH {epoch + 1}")
         model.train()
-        inputs = inputs.to(device)
+
+        input_most_recent_values = input_most_recent_values.to(device)
+        input_rep_history_for_users = input_rep_history_for_users.to(device)
+
         expected_output = expected_output.to(device)
 
-        output = model(inputs)
+        output = model(input_most_recent_values, input_rep_history_for_users)
         print(f"Training desired: {expected_output}, predicted: {output}")
 
         loss = criterion(output, expected_output)
@@ -88,36 +106,46 @@ def train(
 
         model.eval()
         with torch.no_grad():
-            output = model(inputs)
+            output = model(input_most_recent_values, input_rep_history_for_users)
 
         print()  # add a line between epochs
 
 
 def main():
-    """
-    INPUT: (float, float)
-    - First number is the value that the neighbor reported
-    - Second number is the ground truth of whether the PU
-        is there (0 for no, 1 for yes)
-    OUTPUT: float
-    - Probability that the neighbor is malicious
-    """
     from json import load
     from os.path import join
     from feature_select import make_features
-    
+
     with open(join("outfile")) as f:
         raw_data = load(f)["data"]
-        inputs = make_features(raw_data)
+        input_most_recent_values, input_rep_history_for_users = make_features(raw_data)
         expected_output = np.array([1.0 if raw_data[-1]["pu_present"] else 0.0])
+
+    input_most_recent_values = torch.tensor(
+        input_most_recent_values, dtype=torch.float32
+    ).to(device)
+    input_rep_history_for_users = torch.tensor(
+        input_rep_history_for_users, dtype=torch.float32
+    ).to(device)
+    expected_output = torch.tensor(expected_output, dtype=torch.float32).to(device)
 
     n_epochs = 100
     n_hidden = 128
     learning_rate = 0.005
-    model = PrimaryUserPresenceNetwork(hidden_size=n_hidden, num_neighbors=len(raw_data[0]["neighbor_measurements"]), num_layers=2).to(device)
+    model = PrimaryUserPresenceNetwork(
+        hidden_size=n_hidden, num_neighbors=len(raw_data[0]["neighbor_measurements"])
+    ).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     criterion = nn.MSELoss().to(device)
-    train(n_epochs, model, optimizer, criterion, inputs, expected_output)
+    train(
+        n_epochs,
+        model,
+        optimizer,
+        criterion,
+        input_most_recent_values,
+        input_rep_history_for_users,
+        expected_output,
+    )
 
 
 if __name__ == "__main__":
