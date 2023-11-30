dfafa7af0094a4246eb4deba8d04c63e075d908c
Malcolm Anderson <malcolminyo@gmail.com>
2023-11-30 12:33:20 -0800

    Results of testing various configs, along with graphs

--- graph_losses.py
+++ graph_losses.py
@@ -3,24 +3,39 @@ from json import load
 from os.path import join, isfile
 from os import listdir
 
+TEST_RESULTS_FOLDER = "test_results_v4"
+
 
 def make_plot(content):
     meta = content["meta"]
-    title = f"Hidden size {meta['n_hidden']} with LR {meta['lr']}"
+    config = f"hidden size {meta['n_hidden']} with LR {meta['lr']}"
+    epoch_losses = [i["loss"] for i in content["epoch_metrics"]]
 
-    try:
-        epoch_losses = [i[1] for i in content["epoch_losses"]]
-    except TypeError:
-        epoch_losses = content["epoch_losses"]
+    epoch_train_accs = [i["train_acc"] for i in content["epoch_metrics"]]
+    epoch_test_accs = [i["test_acc"] for i in content["epoch_metrics"]]
+
+    # Plot losses
     plt.cla()
     plt.plot(epoch_losses)
-    plt.title(title)
+    plt.title(f"Loss for {config}")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
-    plt.savefig(f"hidden-{meta['n_hidden']}-lr-{meta['lr']}.png")
+    plt.savefig(
+        join(TEST_RESULTS_FOLDER, f"loss-{meta['n_hidden']}-lr-{meta['lr']}.png")
+    )
+
+    # Plot train accuracy
+    plt.cla()
+    plt.ylim(0.0, 1.0)
+    plt.plot(epoch_train_accs, label="Train")
+    plt.plot(epoch_test_accs, label="Test")
+    plt.title(f"Accuracy for {config}")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend(loc="best")
+    plt.savefig(join(TEST_RESULTS_FOLDER, f"acc-{meta['n_hidden']}-lr{meta['lr']}.png"))
 
 
-TEST_RESULTS_FOLDER = "test_results"
 test_result_files = [
     join(TEST_RESULTS_FOLDER, name)
     for name in listdir(TEST_RESULTS_FOLDER)
